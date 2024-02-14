6a61bd955c3ed194c19320d5d4643f3c3b587d5e
MingEn <mingen8216@gmail.com>
2024-02-14 10:16:19 +0100

    fixed bug where BillItems are not cleared and refactored globalUser

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/Expenditure/expenditure.dart';
-import 'screens/memories.dart';
+import 'screens/Memory/memories.dart';
 import 'screens/itinerary.dart';
 import 'screens/saved.dart';
 import 'screens/home.dart';
